71dd565b85410894f9b49f6943e47b6fb96d1774
itsuya <kawa.capri.0101@gmail.com>
2018-10-30 14:15:41 +0900

    varidates in modeles editied, erb and css modified

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
 
-  validates :title, presence: true
+  validates :title,  presence: true, length: { maximum: 30 }
   validates :content, length: { in: 1..500 }
 
   mount_uploader :image, ImageUploader
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :content, length: { in: 1..500 }
 end
